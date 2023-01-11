e446a83ae279168d8a4709c76db63a3334233b1e
nikitanevskij <bike.rental@yandex.by>
2023-01-12 00:10:22 +0300

    fix navigate to home

--- src/components/Header/HeaderStore.js
+++ src/components/Header/HeaderStore.js
@@ -4,26 +4,37 @@ import { BankOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { useAppDispatch } from '../../redux/store';
 import { fetchTools } from '../../redux/fetchToolsSlice';
-
+import { useNavigate } from 'react-router-dom';
 export const HeaderStore = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [selectMenu, setSelectMenu] = React.useState(['home']);
 
   const menuItems = [
     { key: 'home', label: <Link to="/">Главная</Link> },
     { key: 'favorites', label: <Link to="/favorites">Избранное</Link> },
     { key: 'joke', label: 'Alfa joke', icon: <BankOutlined /> },
   ];
-
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       <h2
         style={{ fontSize: '24px', color: 'white', fontWeight: '700', cursor: 'pointer' }}
-        onClick={() => dispatch(fetchTools())}
+        onClick={() => {
+          dispatch(fetchTools());
+          navigate('/');
+          setSelectMenu(['home']);
+        }}
       >
         HARDWARE STORE
       </h2>
 
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['home']} items={menuItems} />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        items={menuItems}
+        selectedKeys={selectMenu}
+        onClick={(item) => setSelectMenu(item.keyPath)}
+      />
     </div>
   );
 };
